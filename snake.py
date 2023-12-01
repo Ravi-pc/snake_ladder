c46a173019cc61825773122eccc4f4ead64a42b0
“Ravi-pc” <ravisingh98138@gmail.com>
2023-12-01 11:57:26 +0530

    [Ravi] Add: Program to genrate numbers of a dice

--- snake.py
+++ snake.py
@@ -13,17 +13,30 @@
 import random
 
 
+def dice_throw():
+    """
+            Description: dice_throw function to generate number between 0 and 6.
+
+            Parameter: None
+
+            Return: Numbers between
+
+    """
+    return random.randint(1, 6)
+
+
 def snake_ladder():
     """
             Description: Snake ladder problem.
 
             Parameter: None
 
-            Return:
+            Return: Zero
 
-         """
+    """
     return 0
 
 
 if __name__ == "__main__":
     print(f'Starting position is {snake_ladder()}')
+    print(f'Dice throw Number {dice_throw()}')
