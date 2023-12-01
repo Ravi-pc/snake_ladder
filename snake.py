78894e3a5bdaad2408827d3e67d070d91fbf8f5f
“Ravi-pc” <ravisingh98138@gmail.com>
2023-12-01 12:35:36 +0530

    [Ravi] Add: Program to check the play condition

--- snake.py
+++ snake.py
@@ -13,6 +13,18 @@
 import random
 
 
+def play(dice):
+    position = 0
+    check_play = random.randint(0, 2)
+    if check_play == 1:
+        position = position
+    elif check_play == 2:
+        position += dice
+    else:
+        position -= dice
+    print(f'current position {position}')
+
+
 def dice_throw():
     """
             Description: dice_throw function to generate number between 0 and 6.
@@ -22,7 +34,10 @@ def dice_throw():
             Return: Numbers between
 
     """
-    return random.randint(1, 6)
+
+    dice = random.randint(1, 6)
+    play(dice)
+    return dice
 
 
 def snake_ladder():
