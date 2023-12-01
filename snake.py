3d5930957e78c85974a248d2d3cac5dbe4c59434
“Ravi-pc” <ravisingh98138@gmail.com>
2023-12-01 11:37:21 +0530

    [Ravi] Add: Program to set the starting position of player to zero

---
+++ snake.py
@@ -0,0 +1,29 @@
+"""
+@Author: Ravi Singh
+
+@Date: 2023-1-12 11:20:30
+
+@Last Modified by:
+
+@Last Modified time: 2023-1-12 12:20:30
+
+@Title : Employee Wage Calculator
+"""
+
+import random
+
+
+def snake_ladder():
+    """
+            Description: Snake ladder problem.
+
+            Parameter: None
+
+            Return:
+
+         """
+    return 0
+
+
+if __name__ == "__main__":
+    print(f'Starting position is {snake_ladder()}')
