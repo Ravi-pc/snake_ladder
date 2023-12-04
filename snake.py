47b11e36b7abbbfe3b0214e53dfb4ed462110234
“Ravi-pc” <ravisingh98138@gmail.com>
2023-12-04 13:30:04 +0530

    [Ravi] Add: Program to win only when position is equals to zero

--- snake.py
+++ snake.py
@@ -5,7 +5,7 @@
 
 @Last Modified by:
 
-@Last Modified time: 2023-4-12 12:25:30
+@Last Modified time: 2023-4-12 1:25:30
 
 @Title : Employee Wage Calculator
 """
@@ -17,21 +17,27 @@ def play(position):
     while position < 100:
         dice = random.randint(1, 6)
         play_condition = random.randint(0, 2)
-        match play_condition:
-            case 0:
-                position += 0
-                print(f'Player stays at the position ')
-            case 1:
-                position += dice
-                print(f'Player moves {dice} position ahead and is at {position} position')
-            case 2:
+        if play_condition == 0:
+            position += 0
+            print(f'Player stays at the same position.')
+        elif play_condition == 1:
+            position += dice
+            if position < 100:
+                print(f'Player moves {dice} position ahead.')
+            elif position > 100:
+                print(f'Extra numbers player cannot move move. ')
+                position -= dice
+            else:
+                print(f'Player moves {dice} position ahead.')
+                print(f'Player Wins!!!!!')
+        else:
+            if position >= 0 and position >= dice:
+                position -= dice
+                print(f'Player moves {dice} position backward.')
                 if position <= 0:
+                    print(f'Sorry!! You need to restart.')
                     position = 0
-                    print(f'Sorry you need to restart!!!!!!!!!!')
-                elif position >= dice:
-                    position -= dice
-                    print(f'Player moves {dice} position backward is at {position} position')
-
+        print(f'Player is at {position} position.')
 
 if __name__ == "__main__":
     start_position = 0
