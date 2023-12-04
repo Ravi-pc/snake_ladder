5ec03161e6b344229d6d4bf7ecb0c87981fea119
“Ravi-pc” <ravisingh98138@gmail.com>
2023-12-04 12:26:00 +0530

    [Ravi] Add: Program to complete the winning condition

--- snake.py
+++ snake.py
@@ -5,7 +5,7 @@
 
 @Last Modified by:
 
-@Last Modified time: 2023-1-12 12:20:30
+@Last Modified time: 2023-4-12 12:25:30
 
 @Title : Employee Wage Calculator
 """
@@ -14,22 +14,23 @@ import random
 
 
 def play(position):
-    while position <= 100:
+    while position < 100:
         dice = random.randint(1, 6)
-        while position < 100:
-            play_condition = random.randint(0, 2)
-            match play_condition:
-                case 0:
-                    position += 0
-                    print(f'Player stays at the position ')
-                case 1:
-                    position += dice
-                    print(f'Player moves {dice} position ahead and is at {position} position')
-                case 2:
-                    if position != 0:
-                        position -= dice
-                        print(f'Player moves {dice} position backward is at {position} position')
-
+        play_condition = random.randint(0, 2)
+        match play_condition:
+            case 0:
+                position += 0
+                print(f'Player stays at the position ')
+            case 1:
+                position += dice
+                print(f'Player moves {dice} position ahead and is at {position} position')
+            case 2:
+                if position <= 0:
+                    position = 0
+                    print(f'Sorry you need to restart!!!!!!!!!!')
+                elif position >= dice:
+                    position -= dice
+                    print(f'Player moves {dice} position backward is at {position} position')
 
 
 if __name__ == "__main__":
